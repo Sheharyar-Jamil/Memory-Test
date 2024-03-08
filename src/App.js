650b09be14ab2639f79375d20af79d0344cfd091
unknown <Sheharyar Jamil>
2024-03-08 20:46:48 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,203 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const initialCard = [
+  {
+    id: 5423,
+    obj: "⚽",
+  },
+
+  {
+    id: 8723,
+    obj: "☂️",
+  },
+
+  {
+    id: 7612,
+    obj: "📙",
+  },
+
+  {
+    id: 9034,
+    obj: "🍵",
+  },
+
+  {
+    id: 8542,
+    obj: "👞",
+  },
+
+  {
+    id: 6666,
+    obj: "⚽",
+  },
+
+  {
+    id: 3906,
+    obj: "☂️",
+  },
+
+  {
+    id: 3456,
+    obj: "👞",
+  },
+
+  {
+    id: 8548,
+    obj: "🚴‍♂️",
+  },
+
+  {
+    id: 6646,
+    obj: "👓",
+  },
+
+  {
+    id: 6906,
+    obj: "🍵",
+  },
+
+  {
+    id: 3556,
+    obj: "🌴",
+  },
+
+  {
+    id: 8532,
+    obj: "📙",
+  },
+
+  {
+    id: 6696,
+    obj: "🚴‍♂️",
+  },
+
+  {
+    id: 3806,
+    obj: "🌴",
+  },
+
+  {
+    id: 3676,
+    obj: "👓",
+  },
+];
 
 function App() {
+  const [selectedObj, setSelectedObj] = useState(null);
+  const [prevSelectedObj, setPrevSelectedObj] = useState(null);
+  const [move, setMove] = useState(0);
+  const [timer, setTimer] = useState(0);
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    let intervalID;
+    if (timerStarted) {
+      intervalID = setInterval(() => {
+        setTimer((timer) => timer + 1);
+      }, 1000);
+    }
+    return () => clearInterval(intervalID);
+  }, [timerStarted]);
+
+  function handleSelectObject(id, obj) {
+    console.log(id, obj);
+    if (obj === prevSelectedObj) {
+      setScore(function (score) {
+        return score + 1;
+      });
+    }
+    setSelectedObj(id !== selectedObj ? id : null);
+    setMove(function (move) {
+      return move + 1;
+    });
+    setPrevSelectedObj(obj);
+
+    setTimerStarted(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container center">
+      <div className="move-timer">
+        <Timer timer={timer} />
+        <Move move={move} />
+      </div>
+      <Score score={score} />
+      <MemoryTest />
+      <Card onSelectedObj={handleSelectObject} selectedObj={selectedObj} />
     </div>
+  );
+}
+
+function MemoryTest() {
+  return (
+    <>
+      <h1 className="heading-primary">Memory Test</h1>
+    </>
+  );
+}
+
+function Card({ onSelectedObj, selectedObj }) {
+  const cards = initialCard;
+  return (
+    <div className="card-section">
+      <ul className="card ">
+        {cards.map(function (card) {
+          return (
+            <Cards
+              cardObj={card}
+              key={card.id}
+              onSelectedObj={onSelectedObj}
+              selectedObj={selectedObj}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function Cards({ cardObj, onSelectedObj, selectedObj }) {
+  const isSelected = selectedObj === cardObj.id;
+  return (
+    <>
+      <li
+        className="card-list"
+        onClick={() => onSelectedObj(cardObj.id, cardObj.obj)}
+      >
+        {isSelected && cardObj.obj}
+      </li>
+    </>
+  );
+}
+
+function Move({ move }) {
+  return (
+    <>
+      <h3 className="heading-tertiary">
+        Moves <span className="span-info">{move}</span>
+      </h3>
+    </>
+  );
+}
+
+function Timer({ timer }) {
+  return (
+    <>
+      <h3 className="heading-tertiary">
+        Time <span className="span-info"> {timer}</span>
+      </h3>
+    </>
+  );
+}
+
+function Score({ score }) {
+  return (
+    <>
+      <h3 className="heading-tertiary">
+        Your Score <span className="span-info">{score}</span>
+      </h3>
+    </>
   );
 }
 
